32b6520aae83c26460c692cfa46f55b4579700e8
totuworld <totuworld@mobidix.co.kr>
2013-07-12 01:23:28 +0900

    Android project edited

--- IAB_test/src/com/totuworld/AndroidIAB/AndroidIABActivity.java
+++ IAB_test/src/com/totuworld/AndroidIAB/AndroidIABActivity.java
@@ -2,16 +2,39 @@ package com.totuworld.AndroidIAB;
 
 import com.unity3d.player.*;
 import android.app.Activity;
+
 import android.content.res.Configuration;
 import android.os.Bundle;
+
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+// for android IAB
+import android.content.Intent;
+import android.util.Log;
+
+import com.android.vending.billing.IabHelper;
+import com.android.vending.billing.IabResult;
+import com.android.vending.billing.Inventory;
+import com.android.vending.billing.Purchase;
+
 public class AndroidIABActivity extends Activity
 {
 	private UnityPlayer mUnityPlayer;
+
+    // Debug tag, for logging
+    static final String TAG = "AndroidBilling";
+
+    // SKUs for our products: your In-app item ID
+    static final String SKU_ID = "IAB_item_ID";
+
+    // (arbitrary) request code for the purchase flow
+    static final int RC_REQUEST = 10001;
+
+    // The helper object
+    IabHelper mHelper;
 
 	// UnityPlayer.init() should be called before attaching the view to a layout. 
 	// UnityPlayer.quit() should be the last thing called; it will terminate the process and not return.
@@ -34,11 +57,51 @@ public class AndroidIABActivity extends Activity
 		View playerView = mUnityPlayer.getView();
 		setContentView(playerView);
 		playerView.requestFocus();
+
+        String base64EncodedPublicKey = "CONSTRUCT_YOUR_KEY_AND_PLACE_IT_HERE";
+
+        // Some sanity checks to see if the developer (that's you!) really followed the
+        // instructions to run this sample (don't put these checks on your app!)
+        if (base64EncodedPublicKey.contains("CONSTRUCT_YOUR")) {
+            throw new RuntimeException("Please put your app's public key in MainActivity.java. See README.");
+        }
+
+        // Create the helper, passing it our context and the public key to verify signatures with
+        Log.d(TAG, "Creating IAB helper.");
+        mHelper = new IabHelper(this, base64EncodedPublicKey);
+
+        // enable debug logging (for a production application, you should set this to false).
+        mHelper.enableDebugLogging(true);
+
+        // Start setup. This is asynchronous and the specified listener
+        // will be called once setup completes.
+        Log.d(TAG, "Starting setup.");
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "Setup finished.");
+
+                if (!result.isSuccess()) {
+                    // Oh noes, there was a problem.
+//                    complain("Problem setting up in-app billing: " + result);
+                    return;
+                }
+                // Hooray, IAB is fully set up. Now, let's get an inventory of stuff we own.
+                Log.d(TAG, "Setup successful. Querying inventory.");
+                mHelper.queryInventoryAsync(mGotInventoryListener);
+            }
+        });
 	}
+
+    // We're being destroyed. It's important to dispose of the helper here!
 	protected void onDestroy ()
 	{
 		super.onDestroy();
 		mUnityPlayer.quit();
+
+        // very important:
+        Log.d(TAG, "Destroying helper.");
+        if (mHelper != null) mHelper.dispose();
+        mHelper = null;
 	}
 
 	// onPause()/onResume() must be sent to UnityPlayer to enable pause and resource recreation on resume.
@@ -78,4 +141,153 @@ public class AndroidIABActivity extends Activity
 	{
 		return mUnityPlayer.onKeyUp(keyCode, event);
 	}
+
+
+
+    public void AnswerToUnity()
+    {
+        Log.d(TAG, "Buy purchase button clicked.");
+
+        // We will be notified of completion via mPurchaseFinishedListener
+
+        /* TODO: for security, generate your payload here for verification.
+         * See the comments on verifyDeveloperPayload() for more info.
+         * Since this is a SAMPLE, we just use an empty string,
+         * but on a production app you should carefully generate this. */
+        String payload = "";
+
+        mHelper.launchPurchaseFlow(this, SKU_ID, RC_REQUEST,
+                mPurchaseFinishedListener, payload);
+    }
+
+
+
+    /// /// /// /// /// ///
+    /// extra IAB method
+    ///
+
+    // Listener that's called when we finish querying the items and subscriptions we own
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+            if (result.isFailure()) {
+//                complain("Failed to query inventory: " + result);
+                return;
+            }
+
+            Log.d(TAG, "Query inventory was successful.");
+
+            /*
+             * Check for items we own. Notice that for each purchase, we check
+             * the developer payload to see if it's correct! See
+             * verifyDeveloperPayload().
+             */
+
+            // Check for gas delivery -- if we own gas, we should fill up the tank immediately
+            Purchase gasPurchase = inventory.getPurchase(SKU_ID);
+            if (gasPurchase != null && verifyDeveloperPayload(gasPurchase)) {
+                Log.d(TAG, "We have gas. Consuming it.");
+                mHelper.consumeAsync(inventory.getPurchase(SKU_ID), mConsumeFinishedListener, AndroidIABActivity.this);
+                return;
+            }
+
+            Log.d(TAG, "Initial inventory query finished; enabling main UI.");
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + "," + data);
+
+        // Pass on the activity result to the helper for handling
+        if (!mHelper.handleActivityResult(requestCode, resultCode, data)) {
+            // not handled, so handle it ourselves (here's where you'd
+            // perform any handling of activity results not related to in-app
+            // billing...
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+        else {
+            Log.d(TAG, "onActivityResult handled by IABUtil.");
+        }
+    }
+
+    /** Verifies the developer payload of a purchase. */
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
+
+        /*
+         * TODO: verify that the developer payload of the purchase is correct. It will be
+         * the same one that you sent when initiating the purchase.
+         *
+         * WARNING: Locally generating a random string when starting a purchase and
+         * verifying it here might seem like a good approach, but this will fail in the
+         * case where the user purchases an item on one device and then uses your app on
+         * a different device, because on the other device you will not have access to the
+         * random string you originally generated.
+         *
+         * So a good developer payload has these characteristics:
+         *
+         * 1. If two different users purchase an item, the payload is different between them,
+         *    so that one user's purchase can't be replayed to another user.
+         *
+         * 2. The payload must be such that you can verify it even when the app wasn't the
+         *    one who initiated the purchase flow (so that items purchased by the user on
+         *    one device work on other devices owned by the user).
+         *
+         * Using your own server to store and verify developer payloads across app
+         * installations is recommended.
+         */
+
+        return true;
+    }
+
+    // Callback for when a purchase is finished
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+            if (result.isFailure()) {
+//                complain("Error purchasing: " + result);
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+//                complain("Error purchasing. Authenticity verification failed.");
+                return;
+            }
+
+            Log.d(TAG, "Purchase successful.");
+
+            if (purchase.getSku().equals(SKU_ID)) {
+                // bought 1/4 tank of gas. So consume it.
+                Log.d(TAG, "Purchase is gas. Starting gas consumption.");
+                mHelper.consumeAsync(purchase, mConsumeFinishedListener, AndroidIABActivity.this);
+            }
+
+        }
+    };
+
+    // Called when consumption is complete
+    IabHelper.OnConsumeFinishedListener mConsumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
+        public void onConsumeFinished(Purchase purchase, IabResult result) {
+            Log.d(TAG, "Consumption finished. Purchase: " + purchase + ", result: " + result);
+
+            // We know this is the "gas" sku because it's the only one we consume,
+            // so we don't check which sku was consumed. If you have more than one
+            // sku, you probably should check...
+            if (result.isSuccess()) {
+                // successfully consumed, so we apply the effects of the
+                // item in our
+                // game world's logic, which in our case means filling the gas tank a bit
+                Log.d(TAG, "Consumption successful. Provisioning.");
+
+                // return purchase complete result value to Unity3D
+                //
+                UnityPlayer.UnitySendMessage("UnityCom", "ResultFromAndroid", "Purchase Complete" );
+            }
+            else {
+//                complain("Error while consuming: " + result);
+            }
+
+            Log.d(TAG, "End consumption flow.");
+        }
+    };
 }
